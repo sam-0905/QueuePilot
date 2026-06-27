e586b7bbda60b99ee45340566748859c159279cb
Allan sam <allansam0905@gmail.com>
2026-06-27 17:32:12 +0530

    Imported a Routes and Route from Browser router

--- QueuePilot/src/App.tsx
+++ QueuePilot/src/App.tsx
@@ -1,11 +1,20 @@
 // import { useState } from 'react'
 
+import { Routes ,Route } from 'react-router-dom';
 import './App.css'
 import Inbox from './Pages/inbox';
+import Header from './components/Header';
 
 function App() {
 
-  return <Inbox />
+  return (
+    <>
+      <Header/>
+    <Routes>
+      <Route path="/inbox" element={<Inbox />} />
+    </Routes>
+    </>
+  )
 }
 
-export default App
+export default App;
