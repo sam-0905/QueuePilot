34a6219b5fc33934090eeaa9d7de962a1d8c122a
Allan sam <allansam0905@gmail.com>
2026-06-27 17:19:25 +0530

    Showed a mock conservation list in Inbox

--- QueuePilot/src/Pages/inbox.tsx
+++ QueuePilot/src/Pages/inbox.tsx
@@ -1,10 +1,11 @@
+import ConversationList from "../components/ConversationList";
 
 function Inbox() {
     return (
         <>
         <div>
             <h1>Inbox</h1>
-            
+            <ConversationList />
         </div>
         </>
     )
