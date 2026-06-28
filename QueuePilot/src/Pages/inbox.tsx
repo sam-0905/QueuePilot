35b2208880043ed5067ff9b57ff85a4a65ae5795
Allan sam <allansam0905@gmail.com>
2026-06-28 22:12:54 +0530

    Added a conversationDetails and some CSS

--- QueuePilot/src/Pages/inbox.tsx
+++ QueuePilot/src/Pages/inbox.tsx
@@ -2,18 +2,29 @@ import { useState } from "react";
 import ConversationList from "../components/ConversationList";
 import { conversations } from "../data/conversations";
 import type { Conversation } from "../types/conversation";
+import ConversationDetails from "../components/ConversationDetails";
 
 function Inbox() {
 
-    const [onSelectedConversation, setSelectedConversation] = useState<Conversation | null>(null);
+    const [selectedConversation, setSelectedConversation] = useState<Conversation | null>(null);
 
     return (
         <>
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Inbox</h1>
-            <ConversationList conversations={conversations} onSelectedConversation={setSelectedConversation} />
-            <p>selected:{} {onSelectedConversation ? onSelectedConversation.customerName : "None"}</p>
+        <div className="flex flex-col items-center min-h-screen py-2">
+            <h1 className="text-3xl font-bold text-lime-500 m-4 p-3">Inbox</h1>
+            <div className="flex flex-row justify-center items-start w-full max-w-4xl mt-4">
+                 <div className="flex flex-col w-1/2">
+                      <ConversationList 
+                    conversations={conversations} 
+                    onSelectedConversation={setSelectedConversation} />
+                 </div>
+                    <div className="flex flex-col w-1/2">
+                          <ConversationDetails 
+                         selectedConversation={selectedConversation} />
+                        </div>
         </div>
+            </div>
+
         </>
     )
 
