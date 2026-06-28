0b0a7e91b9bc8d712b6163cf97343a896332e1af
Allan sam <allansam0905@gmail.com>
2026-06-28 17:27:16 +0530

    In this we just get a data through props and just displayed on UI

---
+++ QueuePilot/src/components/ConversationCard.tsx
@@ -0,0 +1,24 @@
+
+interface ConversationCardProps {  
+    conversation: Conversation;
+}
+
+const ConversationCard = ({ conversation }: ConversationCardProps) => {
+  return (
+    <>
+    <h1>Conversation Card</h1>
+    {
+                <div key={conversation.id}>
+                    <h2>{conversation.customerName}</h2>
+                    <h2>{conversation.status}</h2>
+                    <h3>{conversation.priority}</h3>
+                    <h4>{conversation.waitingTime}</h4>
+                    <p>{conversation.escalationReason}</p>
+                    <p>{conversation.aiSummary}</p>
+                </div>
+        }
+   </>
+  )
+}
+
+export default ConversationCard;
