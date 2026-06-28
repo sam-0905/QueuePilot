7cd198726221dce168ae3ba900dc0a73b1de13b1
Allan sam <allansam0905@gmail.com>
2026-06-28 22:17:35 +0530

    Displayed some data of selectedConversation

--- QueuePilot/src/components/ConversationDetails.tsx
+++ QueuePilot/src/components/ConversationDetails.tsx
@@ -18,7 +18,16 @@ function ConversationDetails({ selectedConversation }: ConversationDetailsProps)
     return (
        
         <>
-           
+            <section className="flex flex-col m-2 p-4 justify-center items-center border rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold mb-4">Conversation Details</h2>
+            <h2 className="text-lg">{selectedConversation?.customerName}</h2>
+            <h3 className="text-md">{selectedConversation?.priority}</h3>
+            <h3 className="text-md">{selectedConversation?.status}</h3>
+             <h4 className="text-sm">{selectedConversation?.waitingTime}</h4>
+            <p className="text-gray-600">{selectedConversation?.escalationReason}</p>
+            <p className="text-gray-600">{selectedConversation?.aiSummary}</p>
+
+            </section>
         </>
     )
 }
