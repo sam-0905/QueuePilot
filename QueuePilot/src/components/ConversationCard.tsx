a814df0013c9de813fa97fe9d2654fa76735bb54
Allan sam <allansam0905@gmail.com>
2026-06-28 22:13:36 +0530

    Updated some CSS

--- QueuePilot/src/components/ConversationCard.tsx
+++ QueuePilot/src/components/ConversationCard.tsx
@@ -9,15 +9,12 @@ const ConversationCard = ({ conversation, onSelect }: ConversationCardProps) => 
 
   return (
     <>
-    {
-                <div key={conversation.id} className="flex m-2 p-3 justify-center items-center" onClick={() => onSelect(conversation)}>
-                    <div className="flex flex-col m-2 p-4 bg-blue-500 rounded-lg shadow-md ">
+      {
+                <div key={conversation.id} className="flex flex-row m-2 p-3 w-full h-full" onClick={() => onSelect(conversation)}>
+                    <div className="flex flex-col p-4 bg-blue-500 rounded-lg shadow-md text-white w-xs h-auto cursor-pointer hover:bg-blue-600">
                     <h2 className="text-lg font-semibold">{conversation.customerName}</h2>
                     <h2 className="text-md">{conversation.status}</h2>
                     <h3 className="text-sm">{conversation.priority}</h3>
-                    <h4 className="text-xs">{conversation.waitingTime}</h4>
-                    <p className="text-sm">{conversation.escalationReason}</p>
-                    <p className="text-sm">{conversation.aiSummary}</p>
                     </div>
                 </div>
         }
