fdf515c6e97872f41e94ed59dc27bfe5d2df6b17
Allan sam <allansam0905@gmail.com>
2026-06-28 20:03:10 +0530

    feat - From inbox we are getting a props  and again passed inside another ConversationCard

--- QueuePilot/src/components/ConversationList.tsx
+++ QueuePilot/src/components/ConversationList.tsx
@@ -4,15 +4,19 @@ import type { Conversation } from "../types/conversation";
 
 interface ConversationListProps {
     conversations: Conversation[]
+    onSelectedConversation?: (conversation: Conversation) => void;    
 }   
 
-const ConversationList = ({conversations }: ConversationListProps) => {
+const ConversationList = ({conversations, onSelectedConversation }: ConversationListProps) => {
   return (
       <>
+      
+          <h1 className="text-2xl font-bold mb-4">Conversation List</h1>
           {conversations.map((conversation) => (
            <ConversationCard   
            key={conversation.id}
            conversation={conversation}
+           onSelect={onSelectedConversation}
            />  
            ))}
     </>
