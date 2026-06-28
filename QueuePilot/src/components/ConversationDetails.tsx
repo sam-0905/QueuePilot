4de3d2c41129abb279dd4bcf7dde781ee734f623
Allan sam <allansam0905@gmail.com>
2026-06-28 22:14:11 +0530

    Styles - Updated h1 Css

---
+++ QueuePilot/src/components/ConversationDetails.tsx
@@ -0,0 +1,45 @@
+import type { Conversation } from "../types/conversation";
+
+interface ConversationDetailsProps {
+    selectedConversation: Conversation | null;
+}
+
+function ConversationDetails({ selectedConversation }: ConversationDetailsProps) {
+
+    if (!selectedConversation) {
+        return (
+        <section className="flex flex-col m-1 justify-center items-center">
+            <h2 className="text-2xl font-bold mb-4">Conversation Details</h2>
+            <p className="text-gray-600"> 👈 Select a conversation to view details. </p>
+            <p> </p>
+        </section>  
+        )}
+
+    return (
+       
+        <>
+            <section className="flex flex-col m-2 p-4 justify-center items-center border rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold mb-4">Conversation Details</h2>
+            <h2 className="text-lg">{selectedConversation?.customerName}</h2>
+            <h3 className="text-md">{selectedConversation?.priority}</h3>
+            <h3 className="text-md">{selectedConversation?.status}</h3>
+             <h4 className="text-sm">{selectedConversation?.waitingTime}</h4>
+            <p className="text-gray-600">{selectedConversation?.escalationReason}</p>
+            <p className="text-gray-600">{selectedConversation?.aiSummary}</p>
+
+            <div>
+                {selectedConversation?.messages?.map((message) => (
+                    <div key={message.id} className="flex flex-col m-2 p-3 justify-center items-center">
+                        <p className="font-bold">{message.sender}</p>
+                        <p className="text-gray-600">{message.text}</p>
+                        <p className="text-sm text-gray-500">{message.timestamp}</p>
+                    </div>
+                ))}
+            </div>
+
+            </section>
+        </>
+    )
+}
+
+export default ConversationDetails;
