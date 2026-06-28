81e93bfd3e2deb253db96c03d5f06f629131f371
Allan sam <allansam0905@gmail.com>
2026-06-28 20:00:19 +0530

    Feat - Added a new state and passed it to ConversationList

--- QueuePilot/src/Pages/inbox.tsx
+++ QueuePilot/src/Pages/inbox.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import ConversationList from "../components/ConversationList";
 import { conversations } from "../data/conversations";
+import type { Conversation } from "../types/conversation";
 
 function Inbox() {
+
+    const [onSelectedConversation, setSelectedConversation] = useState<Conversation | null>(null);
+
     return (
         <>
-        <div>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Inbox</h1>
-            <ConversationList conversations={conversations} />
+            <ConversationList conversations={conversations} onSelectedConversation={setSelectedConversation} />
+            <p>selected:{} {onSelectedConversation ? onSelectedConversation.customerName : "None"}</p>
         </div>
         </>
     )
