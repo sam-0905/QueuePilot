d858459cf6d08ee63e72ce4fe301d9d4f8714539
Allan sam <allansam0905@gmail.com>
2026-06-28 17:23:59 +0530

    feat -Render a conversation list  in Inbox

--- QueuePilot/src/Pages/inbox.tsx
+++ QueuePilot/src/Pages/inbox.tsx
@@ -1,11 +1,12 @@
 import ConversationList from "../components/ConversationList";
+import { conversations } from "../data/conversations";
 
 function Inbox() {
     return (
         <>
         <div>
             <h1>Inbox</h1>
-            <ConversationList />
+            <ConversationList conversations={conversations} />
         </div>
         </>
     )
