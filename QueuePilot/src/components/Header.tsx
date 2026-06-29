2f1f0a6382ff87dea34d64f8e4f8280b43ce6f47
Allan sam <allansam0905@gmail.com>
2026-06-29 09:51:26 +0530

    Added Heading and linked a inbox

--- QueuePilot/src/components/Header.tsx
+++ QueuePilot/src/components/Header.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <section className="flex justify-between ml-auto border-b-2 bg-yellow-100 ">
+      <div>
+        <h1 className="text-2xl font-bold m-4 ">QueuePilot</h1>
+    </div>
+    <div className="text-xl m-1 p-2">
+      <Link to="/" >Inbox</Link>
+    </div>
+    </section>
+    
+    </>
   )
 }
 
