079378157bd743670b5d5c3eba2ab8530ac2adba
Allan sam <allansam0905@gmail.com>
2026-06-29 09:23:03 +0530

    updated with optional chaining

--- QueuePilot/src/components/ConversationCard.tsx
+++ QueuePilot/src/components/ConversationCard.tsx
@@ -10,7 +10,7 @@ const ConversationCard = ({ conversation, onSelect }: ConversationCardProps) => 
   return (
     <>
       {
-                <div key={conversation.id} className="flex flex-row m-2 p-3 w-full h-full" onClick={() => onSelect(conversation)}>
+                <div key={conversation.id} className="flex flex-row m-2 p-3 w-full h-full" onClick={() => onSelect?.(conversation)}>
                     <div className="flex flex-col p-4 bg-blue-500 rounded-lg shadow-md text-white w-xs h-auto cursor-pointer hover:bg-blue-600">
                     <h2 className="text-lg font-semibold">{conversation.customerName}</h2>
                     <h2 className="text-md">{conversation.status}</h2>
